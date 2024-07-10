8b61d1b7d74165c38d2b016a13ce289ef78d3b2d
Anup Agarwal <anuagarw7@WKMZTB83D1A2.global.publicisgroupe.net>
2024-07-10 11:57:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+import { PublicClientApplication, EventType } from "@azure/msal-browser";
+
+const msalConfig = {
+  auth: {
+    clientId: process.env.REACT_APP_MS_CLIENT_ID,
+    authority: `https://login.microsoftonline.com/${process.env.REACT_APP_MS_TENANT_ID}`,
+    redirectUri: "/",
+    post_logout_redirect_uri: "/",
+  },
+};
+
+const msalInstance = new PublicClientApplication(msalConfig);
+
+const App = () => {
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    const initializeMsal = async () => {
+      await msalInstance.initialize();
+      const result = await msalInstance.handleRedirectPromise();
+
+      if (result && result.account) {
+        console.log(result);
+        setAccount(result.account);
+      } else {
+        const currentAccounts = msalInstance.getAllAccounts();
+        if (currentAccounts.length === 1) {
+          setAccount(currentAccounts[0]);
+        }
+      }
+    };
+
+    msalInstance.addEventCallback((event) => {
+      if (
+        event.eventType === EventType.LOGIN_SUCCESS &&
+        event.payload.account
+      ) {
+        setAccount(event.payload.account);
+      }
+    });
+    initializeMsal();
+  }, []);
+
+  const handleLogin = async () => {
+    await msalInstance.initialize();
+    msalInstance.loginRedirect({
+      scopes: ["user.read", "Offline_access"],
+      response_type: "code",
+    });
+  };
+
+  const handleLogout = async () => {
+    await msalInstance.initialize();
+    msalInstance.logoutRedirect();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to MSAL React App</h1>
+
+        {account ? (
+          <div>
+            <h1>Your info:</h1>
+            <p>{JSON.stringify(account)}</p>
+            <p>Welcome, {account.username}</p>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        ) : (
+          <button onClick={handleLogin}>Login</button>
+        )}
       </header>
     </div>
   );
-}
+};
 
 export default App;
